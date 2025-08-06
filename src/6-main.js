df22c4fc3a54e1ef4f688f4412c8cb34002b61fb
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-08-06 17:22:37 -0300

    agregamos otra forma de usar el export utilizando el commonjs y export

--- src/6-main.js
+++
@@ -1,4 +0,0 @@
-import { multiplicar, dividir } from "./5-nuevasFuncionesMateticas.js";
-
-console.log(multiplicar(5,4))
-console.log(dividir(24,3))
---
+++ src/6-main.mjs
@@ -0,0 +1,4 @@
+import { multiplicar, dividir } from "./5-nuevasFuncionesMateticas.mjs";
+
+console.log(multiplicar(5,4))
+console.log(dividir(24,3))
